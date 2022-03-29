2052f3beadbbf90702e07ec12216821573644d20
militrik <militrik@users.noreply.github.com>
2022-03-29 23:44:00 +0300

    Update runner.ps1

--- runner.ps1
+++ runner.ps1
@@ -2,7 +2,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install -y python3 # Install Python
+choco install -y python3-pip # Install Python
 
 choco install -y git # Install GIT
 
